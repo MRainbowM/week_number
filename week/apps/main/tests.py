ec5478e868f355e7aec64604bd98325cf10e245e
Falileeva Maria <raduga.mf@gmail.com>
2022-06-05 15:45:47 +0800

    Update tests

--- week/apps/main/tests.py
+++ week/apps/main/tests.py
@@ -6,9 +6,6 @@ from .views import get_week_number
 
 
 class WeekTestCase(TestCase):
-    def setUp(self):
-        pass
-
     def test_get_week_number(self):
         self.assertEqual(
             get_week_number(
@@ -24,4 +21,12 @@ class WeekTestCase(TestCase):
                 start_date=datetime.date(2019, 1, 1)
             ),
             2
+        )
+
+        self.assertEqual(
+            get_week_number(
+                input_date=datetime.date(2019, 3, 1),
+                start_date=datetime.date(2019, 1, 1)
+            ),
+            9
         )
